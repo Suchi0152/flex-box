88cf8ab09dea1d5f8ca5f2a9f640335d38264069
KANIGELPULA SUCHITRA <Kanigelpulasuchitra@gmail.com>
2026-05-27 12:32:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ document.body.style.background="skyblue";
 
 });
 
-
 function showText(number){
 
 let text=document.getElementById("displayText");
@@ -14,21 +13,21 @@ let text=document.getElementById("displayText");
 if(number===1){
 
 text.innerHTML=
-"Item 1: Flexbox allows elements to align easily in rows and columns.";
+"Item 1: Flexbox allows easy alignment of webpage elements.";
 
 }
 
 else if(number===2){
 
 text.innerHTML=
-"Item 2: Responsive layouts automatically adjust to screen sizes.";
+"Item 2: Responsive layouts automatically adjust for devices.";
 
 }
 
 else{
 
 text.innerHTML=
-"Item 3: Media queries help websites work on mobile devices.";
+"Item 3: Media queries help websites work on mobile and desktop.";
 
 }
 
